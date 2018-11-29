60c72331369a19102df3d59caca798ece47f0e5e
zero1997 <920246199@qq.com>
2018-11-29 22:49:32 +0800

    第二次提交

--- 4.3.1.1.py
+++ 4.3.1.1.py
@@ -1,2 +1,16 @@
-# -*- coding UTF-8 -*-
-#将特征缩放至特定范围内
+# -*- coding: UTF-8 -*-
+
+#将特征缩放至特定范围内， 将特征缩放到给定的最小值和最大值之间
+
+from sklearn import preprocessing
+import numpy as np   
+
+#创建矩阵
+X_train = np.array([[ 1., -1.,  2.],
+                    [ 2.,  0.,  0.],
+                    [ 0.,  1., -1.]])
+#函数复制
+min_max_scaler = preprocessing.MinMaxScaler()
+#按特征的最小值和最大值放缩到【0， 1】之间，默认0， 1
+X_train_minmax = min_max_scaler.fit_transform(X_train)
+print X_train_minmax
